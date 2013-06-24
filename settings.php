3b73655de97d216780a77c0f1e67e17bf36ae069
Evgeny Karataev <karataev.evgeny@gmail.com>
2013-06-24 08:00:32 +0000

    make repo clean and then try to add some ingores

--- settings.php
+++
@@ -1,8 +0,0 @@
-<?php
-$my_base_url = 'http://localhost';
-$my_pligg_base = '/Colfusion';
-$dblang = 'en';
-define('table_prefix', 'colfusion_');
-$language = 'english';
-include_once mnminclude.'settings_from_db.php';
-?>
