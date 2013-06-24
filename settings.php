b760bfb89083f2523a3c516e3ce99190a025bbed
Evgeny Karataev <karataev.evgeny@gmail.com>
2013-06-24 11:48:32 +0000

    hopefully now brunch has everything

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+$my_base_url = 'http://localhost';
+$my_pligg_base = '/ColfusionBr';
+$dblang = 'en';
+define('table_prefix', 'colfusion_');
+$language = 'english';
+include_once mnminclude.'settings_from_db.php';
+?>
