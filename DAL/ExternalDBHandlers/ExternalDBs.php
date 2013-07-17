cfee5b18b12a23c436e4bfe4319ef355ed397dfd
Evgeny Karataev <karataev.evgeny@gmail.com>
2013-07-17 14:52:24 +0000

    small refactor before implementing doquery from database

--- DAL/ExternalDBHandlers/ExternalDBs.php
+++ DAL/ExternalDBHandlers/ExternalDBs.php
@@ -4,13 +4,19 @@ require_once(realpath(dirname(__FILE__)) . "/DatabaseHandler.php");
 require_once(realpath(dirname(__FILE__)) . "/DatabaseHandlerFactory.php");
 
 class ExternalDBs {
+
+	// Get a database handler by database type which is one of the properties of input parameter.
+	public static function GetDBHandler($externalDBCredentials) {
+		return DatabaseHandlerFactory::createDatabaseHandler($externalDBCredentials->driver, $externalDBCredentials->user_name, $externalDBCredentials->password, $externalDBCredentials->source_database, $externalDBCredentials->server_address, $externalDBCredentials->port);
+	}
+
     public static function GetTableDataBySidAndName($table_name, $perPage, $pageNo, $externalDBCredentials) {
-        $dbHandler = DatabaseHandlerFactory::createDatabaseHandler($externalDBCredentials->driver, $externalDBCredentials->user_name, $externalDBCredentials->password, $externalDBCredentials->source_database, $externalDBCredentials->server_address, $externalDBCredentials->port);
+        $dbHandler = ExternalDBs::GetDBHandler($externalDBCredentials);
         return $dbHandler->getTableData($table_name, $perPage, $pageNo);
     }
 
     public static function GetTotalNumberTuplesInTableBySidAndName($table_name, $externalDBCredentials) {
-        $dbHandler = DatabaseHandlerFactory::createDatabaseHandler($externalDBCredentials->driver, $externalDBCredentials->user_name, $externalDBCredentials->password, $externalDBCredentials->source_database, $externalDBCredentials->server_address, $externalDBCredentials->port);
+        $dbHandler = ExternalDBs::GetDBHandler($externalDBCredentials);
         return $dbHandler->getTotalNumberTuplesInTable($table_name);
     }
 }
