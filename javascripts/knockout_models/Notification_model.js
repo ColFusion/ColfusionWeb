d6ef976214566fed029b824ecea0f1a91406704d
Zhongkai LastName <email@email.com>
2013-10-11 00:10:36 +0000

    [Bug #165] Urls for "numbers of notifications" and "story related to notifications" have been corrected.

--- javascripts/knockout_models/Notification_model.js
+++ javascripts/knockout_models/Notification_model.js
@@ -14,8 +14,14 @@ function NotificationViewModel() {
     self.ntfs = ko.observableArray();
     self.ntf_id = ko.observable();
     self.ntfsNUM = ko.observable();
+    self.visible = ko.observable(false);
+
+    self.displayMore = function(){
+        self.visible(!self.visible());
+    }
+
     $.ajax({
-        url: 'notificationController.php?action=allUserNTF',
+        url: '/Colfusion/notification/notificationController.php?action=allUserNTF',
         type: 'get',
         dataType: 'json',
         success: function (data) {
@@ -30,7 +36,7 @@ function NotificationViewModel() {
     });
 
     self.goToStory = function(ntf) {
-        story_url = "../story.php?title="+ntf.target_id;
+        story_url = "/Colfusion/story.php?title="+ntf.target_id;
         //story_url = "http://localhost/Colfusion/story.php?title="+ntf.target_id;
         window.open(story_url);
     }
@@ -40,7 +46,7 @@ function NotificationViewModel() {
 function GetCurrentNTFNum(){
     var number = 0;
     $.ajax({
-        url: '../notification/notificationController.php?action=getNTFnum',
+        url: '../Colfusion/notification/notificationController.php?action=getNTFnum',
         type: 'get',
         dataType: 'json',
         success: function (data) {
@@ -50,3 +56,21 @@ function GetCurrentNTFNum(){
         }
     });
 }
+
+ko.bindingHandlers.ntfVisible = {
+    init: function(element, valueAccessor) {
+        // Initially set the element to be instantly visible/hidden depending on the value
+        var value = valueAccessor();
+        $(element).toggle(ko.utils.unwrapObservable(value)); // Use "unwrapObservable" so we can handle values that may or may not be observable
+    },
+    update: function(element, valueAccessor) {
+        // Whenever the value subsequently changes, slowly fade the element in or out
+        var value = valueAccessor();
+        ko.utils.unwrapObservable(value) ? $(element).fadeIn() : $(element).fadeOut();
+    }
+};
+
+function applyb() {
+    ko.applyBindings(new NotificationViewModel);
+}
+
