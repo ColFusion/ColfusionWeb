c644fcdbcd17206637bb8f0d2dd24ffbf33fe11b
Xinzhu Zhang <eamil@email.com>
2013-11-19 00:37:40 +0000

    refs #195. Modify CSS style for new pop-up chat room.

--- chat/chatroom.php
+++ chat/chatroom.php
@@ -5,7 +5,7 @@
 <title>Group chat</title>
 
 <link rel="stylesheet" type="text/css" href="../css/jScrollPane.css" />
-<link rel="stylesheet" type="text/css" href="../css/chat.css" />
+<link rel="stylesheet" type="text/css" href="../css/chatRoom.css" />
 
 </head>
 
@@ -15,27 +15,30 @@
 	echo $_GET['room'];
 ?>;
 </script>
-<div id="chatContainer">
 
-    <div id="chatLineHolder"></div>
+<div id="chatBody">
+	<!-- Chat content -->
+	<div id="chatContainer">
+		<div id="chatLineHolder"></div>
 
-	<div id="sidebar-header">
-	    <ul>
-			<li class="selected"><a>Online Users</a></li>
-		</ul>
+		<div id="chatBottomBar">
+			<div class="tip"></div>
+			<div id="submitForm">
+				<input id="chatText" name="chatText" maxlength="255" />
+				<input type="submit" id="submitButton" value="Submit" />
+			</div>
+		</div>
 	</div>
-	<div id="chatUsers" class="rounded"></div>
-	
 
-    <div id="chatBottomBar" class="rounded">
-    	<div class="tip"></div>
-	    <div id="submitForm">
-            <input id="chatText" name="chatText" class="rounded" maxlength="255" />
-            <input type="submit" id="submitButton" value="Submit" />
-        </div>
-        
-    </div>
-    
+	<!-- Online user list -->
+	<div id="chatSide">
+		<div id="sidebar-header">
+			<ul>
+				<li class="selected"><a>Online Users</a></li>
+			</ul>
+		</div>
+		<div id="chatUsers"></div>
+	</div>
 </div>
 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
@@ -44,6 +47,3 @@
 <script src="../javascripts/knockout_models/Chat_model.js"></script>
 </body>
 </html>
-
-
-
