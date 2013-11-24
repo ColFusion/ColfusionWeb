5ef9af89af062c8c42a8aba3517f41f5e635cf44
Evgeny Karataev <karataev.evgeny@gmail.com>
2013-11-24 03:56:37 +0000

    added sql file with trigger, now ktr file has name tag which is set up to eid from the executeinfo table refs #173

--- database_scripts/ADD_TRIGGERS_ON_TRANSFORMATION.sql
+++ database_scripts/ADD_TRIGGERS_ON_TRANSFORMATION.sql
@@ -4,18 +4,16 @@ CREATE
 	TRIGGER `transformation_after_insert` AFTER INSERT 
 	ON `colfusion_pentaho_log_transformaion` 
 	FOR EACH ROW 
-        BEGIN
-	        SET @st = (SELECT status FROM colfusion_pentaho_log_transformaion where TRANSNAME = NEW.TRANSNAME);
-
-	        UPDATE `colfusion_executeinfo`
-            SET status = 
-                CASE 
-                   WHEN @st like '%end%' THEN
-                    'success'
-                	ELSE @st
-                END
+    BEGIN
+	      IF NEW.status like '%end%' THEN
+    		UPDATE `colfusion_executeinfo`
+            SET status = 'success', RecordsProcessed = NEW.LINES_WRITTEN
             WHERE Eid = NEW.TRANSNAME;
-		
+    	ELSE
+    		UPDATE `colfusion_executeinfo`
+        	SET status = NEW.status
+        	WHERE Eid = NEW.TRANSNAME;
+    	END IF;
     END$$
 
 DELIMITER ;
@@ -27,18 +25,16 @@ CREATE
 	TRIGGER `transformation_after_update` AFTER UPDATE 
 	ON `colfusion_pentaho_log_transformaion` 
 	FOR EACH ROW 
-        BEGIN
-        	SET @st = (SELECT status FROM colfusion_pentaho_log_transformaion where TRANSNAME = NEW.TRANSNAME);
-
-	        UPDATE `colfusion_executeinfo`
-            SET status = 
-                CASE 
-                   WHEN @st like '%end%' THEN
-                    'success'
-                	ELSE @st
-                END
+    BEGIN
+    	IF NEW.status like '%end%' THEN
+    		UPDATE `colfusion_executeinfo`
+            SET status = 'success', RecordsProcessed = NEW.LINES_WRITTEN
             WHERE Eid = NEW.TRANSNAME;
-		
+    	ELSE
+    		UPDATE `colfusion_executeinfo`
+        	SET status = NEW.status
+        	WHERE Eid = NEW.TRANSNAME;
+    	END IF;
     END$$
 
 DELIMITER ;
