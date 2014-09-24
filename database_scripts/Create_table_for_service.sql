84668e7c1465b75c8df2d2e7647d53a2728983b4
Bai Hao <email@email.com>
2014-09-24 23:25:58 +0000

    modified table refs # 549

--- database_scripts/Create_table_for_service.sql
+++ database_scripts/Create_table_for_service.sql
@@ -1,10 +1,11 @@
-Create table colfusion_service(
-	serviceID int not null,
-	serviceName varchar(20),
-	serviceAddress varchar (30) not null,
-	portNumber int not null,
-	serviceDir char(100) not null,
-	serviceCommand varchar (100) not null,
-	serviceStatus varchar (10) not null,
-	primary key(serviceName)
+create table colfusion_service(
+	service_id int not null,
+	service_name varchar(20) collate utf8_unicode_ci,
+	service_address varchar (30) collate utf8_unicode_ci not null,
+	port_number int not null,
+	service_dir varchar(100) collate utf8_unicode_ci not null,
+	service_command varchar (100) collate utf8_unicode_ci not null,
+	service_status varchar (20) collate utf8_unicode_ci not null,
+	primary key(service_name)
 )
+
