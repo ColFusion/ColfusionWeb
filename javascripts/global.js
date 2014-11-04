9543ae0b8ef310d69fb5865b7d5894fd25ad5ab1
Evgeny Karataev <karataev.evgeny@gmail.com>
2014-11-04 00:16:43 +0000

    refs #413 Updated ColFusionServer rest URL since it was changed in web.xml because of swagger docs

--- javascripts/global.js
+++ javascripts/global.js
@@ -1,4 +1,4 @@
 var ColFusionDomain = "http://localhost";
 var ColFusionAppName = "Colfusion";
-var ColFusionServerUrl = "http://localhost/REST/ColFusionServer";
+var ColFusionServerUrl = "http://localhost/REST/ColFusionServer/rest";
 var OpenRefineUrl = "http://localhost/OpenRefine";
