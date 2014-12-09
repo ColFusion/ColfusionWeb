1c9d5f05cfeb2482d6f0b0fd1ea8c7aaf256cad0
Bai Hao <email@email.com>
2014-12-09 21:45:36 +0000

    refs # 628

--- javascripts/knockout_models/serviceMonitorViewModel.js
+++ javascripts/knockout_models/serviceMonitorViewModel.js
@@ -2,26 +2,44 @@ function ColfusionServicesViewModel() {
     var self = this;
     
     self.servicesList = ko.observableArray();
+    self.serviceStatus = ko.observable("");
     
-    self.fetchServicesLists = function() {
+    self.getServicesList = function() {
         $.ajax({
             url: ColFusionServiceMonitorUrl + "/ServiceMonitor/getServicesStatus",
             type: 'GET',
             dataType: 'json',
-            ontentType: "application/json",
-            rossDomain: true,
+            contentType: "application/json",
+            crossDomain: true,
             success: function(data) {
                 for (var i = 0; i < data.length; i++) {
                     self.servicesList.push(data[i]);
                 }
             },
             error: function(data) {
-                alert("Something went wrong while fetching services. Please try again.");
+                alert("Something went wrong while getting services' list. Please try again.");
             }
         });
     };
     
-    self.fetchServicesLists();
+    self.getServiceStatusByID = function(serviceID) {
+    	$.ajax({
+            url: ColFusionServiceMonitorUrl + "/ServiceMonitor/getServiceStatusByID/" + serviceID,
+            type: 'GET',
+            dataType: 'json',
+            contentType: "application/json",
+            crossDomain: true,
+            success: function(data) {
+            	self.serviceStatus = data;
+            },
+            error: function(data) {
+                alert("Something went wrong while getting service's status by its ID. Please try again.");
+            }
+        });
+    };
+    
+    self.getServicesList();
+    
 }
 
 ko.applyBindings(new ColfusionServicesViewModel());
