29da19ac8021aa030632d51596f7d1046839a9d1
Evgeny Karataev <karataev.evgeny@gmail.com>
2014-12-09 22:56:33 +0000

    refs #628

--- javascripts/knockout_models/serviceMonitorViewModel.js
+++ javascripts/knockout_models/serviceMonitorViewModel.js
@@ -23,23 +23,43 @@ function ColfusionServicesViewModel() {
     };
     
     self.getServiceStatusByID = function(serviceID) {
-    	$.ajax({
+        $.ajax({
             url: ColFusionServiceMonitorUrl + "/ServiceMonitor/getServiceStatusByID/" + serviceID,
             type: 'GET',
             dataType: 'json',
             contentType: "application/json",
             crossDomain: true,
             success: function(data) {
-            	self.serviceStatus = data;
+                self.serviceStatus = data;
             },
             error: function(data) {
                 alert("Something went wrong while getting service's status by its ID. Please try again.");
             }
         });
     };
+
+    self.removeService = function(serviceToRemove) {
+        $.ajax({
+            url: ColFusionServiceMonitorUrl + "/ServiceMonitor/deleteServiceByID/" + serviceToRemove.serviceID,
+            type: 'GET',
+            dataType: 'json',
+            contentType: "application/json",
+            crossDomain: true,
+            success: function(data) {
+                self.servicesList.remove(serviceToRemove);
+                alert(data.message);
+            },
+            error: function(data) {
+                alert("Something went wrong while getting service's status by its ID. Please try again.");
+            }
+        });
+    };
+
+    self.editService = function(serviceToRemove) {
+        //TODO: implement
+    };
     
     self.getServicesList();
-    
 }
 
 ko.applyBindings(new ColfusionServicesViewModel());
