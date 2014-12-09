8de0952ae3b759418b71efe204a8a6945681c3f6
Evgeny Karataev <karataev.evgeny@gmail.com>
2014-12-09 04:37:41 +0000

    refs #628

--- javascripts/knockout_models/serviceMonitorViewModel.js
+++ javascripts/knockout_models/serviceMonitorViewModel.js
@@ -1,48 +1,27 @@
 function ColfusionServicesViewModel() {
     var self = this;
-
-    self.isFetchServicesInProgress = ko.observable(false);
-    self.isFetchServicesErrorMessage = ko.observable(false);
     
     self.servicesList = ko.observableArray();
     
     self.fetchServicesLists = function() {
-    	self.isFetchServicesInProgress(true);
-        self.isFetchServicesErrorMessage(false);
-        
-	    $.ajax({
-	        url: ColFusionServiceMonitorUrl + "/ServiceMonitor/getServicesStatus",
-	        type: 'GET',
-	        dataType: 'json',
-	        contentType: "application/json",
-	        crossDomain: true,
-	        success: function(data) {
-		         self.isFetchServicesInProgress(false);
-		         if (data.isSuccessful) {
-		        	 
-			          var payload = data.payload;
-			          var serviceItem;
-			          
-			          for (var i = 0; i < payload.length; i++) {
-			        	  serviceItem = new ColfusionServicesViewModel();
-			        	  serviceItem = payload[i];
-			
-			        	  self.serviceList.push(serviceItem);
-			          };
-		         }
-		         else {
-		              self.isFetchServicesErrorMessage("Something went wrong while fetching services. Please try again.");
-		         }
-	        },
-	        error: function(data) {
-		         self.isFetchServicesInProgress(false);
-		         self.isFetchServicesErrorMessage("Something went wrong while fetching services. Please try again.");
-	        }
-	    });
-    }
+        $.ajax({
+            url: ColFusionServiceMonitorUrl + "/ServiceMonitor/getServicesStatus",
+            type: 'GET',
+            dataType: 'json',
+            ontentType: "application/json",
+            rossDomain: true,
+            success: function(data) {
+                for (var i = 0; i < data.length; i++) {
+                    self.servicesList.push(data[i]);
+                }
+            },
+            error: function(data) {
+                alert("Something went wrong while fetching services. Please try again.");
+            }
+        });
+    };
     
     self.fetchServicesLists();
 }
 
 ko.applyBindings(new ColfusionServicesViewModel());
-
