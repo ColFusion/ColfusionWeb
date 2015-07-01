ce8937c4c3266bb1eb2a1c00b20335cd93c512b2
EvgenyKarataev <karataev.evgeny@gmail.com>
2015-06-30 19:05:20 -0500

    Update rest server url

--- javascripts/global.js
+++ javascripts/global.js
@@ -1,5 +1,5 @@
 var ColFusionDomain = "http://localhost";
 var ColFusionAppName = "Colfusion";
-var ColFusionServerUrl = "http://localhost/REST/ColFusionServer/rest";
+var ColFusionServerUrl = "http://localhost:8080/ColFusionServer/rest"; //"http://localhost/REST/ColFusionServer/rest";
 var OpenRefineUrl = "http://localhost/OpenRefine";
 var ColFusionServiceMonitorUrl = "http://localhost:7473/rest";
