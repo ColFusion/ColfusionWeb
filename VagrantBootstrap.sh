9d639f4835d7ce2f60857d3a519fba9bac588231
EvgenyKarataev <karataev.evgeny@gmail.com>
2015-08-26 18:09:39 -0400

    Add vagrant to run ubuntu and apache server

---
+++ VagrantBootstrap.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y apache2
+
+apt-get install -y php5 libapache2-mod-php5 php5-mcrypt php5-mysql
+
+cp /vagrant/VagrantBootstrapApacheVirtualHostConfig.conf /etc/apache2/sites-available/
+
+cd /etc/apache2/mods-available
+
+a2enmod proxy
+a2enmod proxy_http
+
+cd /etc/apache2/sites-available
+a2dissite 000-default
+a2ensite VagrantBootstrapApacheVirtualHostConfig
+
+
+
+sudo service apache2 restart
+
+if ! [ -L /var/www ]; then
+  rm -rf /var/www
+  ln -fs /vagrant /var/www
+fi
