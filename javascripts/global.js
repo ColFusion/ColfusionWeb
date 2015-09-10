290e660d3a764f10d0911e974493b582fd36b29f
Dan <pittdas@users.noreply.github.com>
2015-09-10 12:39:44 -0400

    add back globals.js

--- javascripts/global.js
+++ javascripts/global.js
@@ -1,5 +1,8 @@
 var ColFusionDomain = "http://localhost";
-var ColFusionAppName = "Colfusion";
+
+//e.g., "/" or "/Colfusion/"
+var ColFusionAppPath = "/";
+
 var ColFusionServerUrl = "http://localhost:8080/ColFusionServer/rest"; //"http://localhost/REST/ColFusionServer/rest";
 var OpenRefineUrl = "http://localhost/OpenRefine";
 var ColFusionServiceMonitorUrl = "http://localhost:7473/rest";
