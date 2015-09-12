b7e480a22aaac4b1a2451f7b4f539486952b8480
Evgeny Karataev <Karataev.Evgeny@gmail.com>
2015-09-11 20:10:02 -0400

    Fixes #12

--- javascripts/global.js
+++ javascripts/global.js
@@ -3,6 +3,6 @@ var ColFusionDomain = "http://192.168.33.10";
 //e.g., "/" or "/Colfusion/"
 var ColFusionAppPath = "/";
 
-var ColFusionServerUrl = "http://192.168.33.10/REST/ColFusionServer/rest"; //"http://localhost:8080/ColFusionServer/rest"; 
+var ColFusionServerUrl = "http://192.168.33.10/REST"; //"http://localhost:8080/ColFusionServer/rest"; 
 var OpenRefineUrl = "http://192.168.33.10/OpenRefine";
 var ColFusionServiceMonitorUrl = "http://192.168.33.10:7473/rest";
