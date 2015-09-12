56d9355826fdc2eb306746ec53108188aaed3e27
Evgeny Karataev <Karataev.Evgeny@gmail.com>
2015-09-11 21:38:41 -0400

    Refs #14 rest api reorganizaiton for dataverse endpoint

--- javascripts/global.js
+++ javascripts/global.js
@@ -6,3 +6,32 @@ var ColFusionAppPath = "/";
 var ColFusionServerUrl = "http://192.168.33.10/REST"; //"http://localhost:8080/ColFusionServer/rest"; 
 var OpenRefineUrl = "http://192.168.33.10/OpenRefine";
 var ColFusionServiceMonitorUrl = "http://192.168.33.10:7473/rest";
+
+
+
+// The following describtes REST API endpoints
+
+var restApis = (function() {
+	var restApis = {};
+
+	var harvardDataverse = "HarvardDataverse";
+
+	restApis.getDataverseDownload = function(id, name, sid, uploadTimestamp) {
+		var url = ColFusionServerUrl + "/" + harvardDataverse + "/Download/" + id + "/" + name + "/" + sid + "/" + uploadTimestamp;
+
+		console.log(url);
+
+		return url;
+	};
+
+	restApis.getDataverseSearch = function(fileName, dataverseName, datasetName) {
+		var url = ColFusionServerUrl + "/" + harvardDataverse + "/Search?dataverseName=" + dataverseName + 
+						"&fileName=" + fileName + "&datasetName=" + datasetName;
+
+		console.log(url);
+
+		return url;
+	};
+
+	return restApis;
+})();
--- templates/wistie/js/wizardFromDataverse.js
+++ templates/wistie/js/wizardFromDataverse.js
@@ -1,111 +1,84 @@
+/**
+ * Download file from Harvard Dataverse to server.
+ * @return {[type]} [description]
+ */
+function downloadRequest() {
+    debugger;
+    $('#uploadTimestamp').val(new Date().getTime()); // Why do we need it?
 
-/*Download file from Harvard Dataverse to server. */
-  function downloadRequest() {
-       
-
-				$('#uploadTimestamp').val(new Date().getTime());
-                $.ajax({
-                    url:ColFusionServerUrl+"/HDataverse/DownLoad/"+$("input[name='fileid']:checked").val()+"/"+$("input[name='fileid']:checked").attr("data-value")+"/"+$("#uploadFormSid").val()+"/"+$("#uploadTimestamp").val(),
-                    type: 'POST',
-                    dataType: 'text',
-                    contentType: "application/json",
-                    crossDomain: true,
-                    success: function(data) {
-						//console.log(data);
-						$('#downloadMessage').text("Upload Successfully!");
-						wizard.enableNextButton();
-						var resultJson = JSON.parse(data);
-                        wizardFromFile.fromComputerUploadFileViewModel.uploadedFileInfos.push(resultJson.payload[0]);
-						wizardFromFile.fromComputerUploadFileViewModel.isUploadSuccessful(resultJson.isSuccessful);
-						wizardFromFile.fromComputerUploadFileViewModel.uploadMessage(resultJson.message);
-                    },
-                    error: function(data) {
-                        alert("Something went wrong while getting services' list. Please try again.");
-                    }
-                });
-
-            
+    //TODO figure exactly what it is and give it a better name
+    var ids = $("input[name='fileid']:checked").val();
+    var name = $("input[name='fileid']:checked").attr("data-value");
+    var sid = $("#uploadFormSid").val();
+    var uploadTimestamp = $("#uploadTimestamp").val(); // Why do we need it?
+	
+    $.ajax({
+        url: restApis.getDataverseDownload(ids, name, sid, uploadTimestamp),
+        type: 'GET',
+        dataType: 'application/json',
+        contentType: 'application/json',
+        success: function(data) {
+			//console.log(data);
+			$('#downloadMessage').text("Upload Successfully!");
+			wizard.enableNextButton();
+			var resultJson = JSON.parse(data);
+            wizardFromFile.fromComputerUploadFileViewModel.uploadedFileInfos.push(resultJson.payload[0]);
+			wizardFromFile.fromComputerUploadFileViewModel.isUploadSuccessful(resultJson.isSuccessful);
+			wizardFromFile.fromComputerUploadFileViewModel.uploadMessage(resultJson.message);
+        },
+        error: function(data) {
+            alert("Something went wrong while getting files list. Please try again.");
         }
+    });
+}
 		
-/*Search files on the Harvard Dataverse*/
-   function searchDataverse() {
-					$('#searchField').hide();
-					$('#searchResult').show();
-					if($('#filenum').text().indexOf("no") > -1){
-						$('#uploadbtn').hide();
-					}
-					if($('#filenum').text().indexOf("0") > -1){
-						$('#uploadbtn').hide();
-					}
-					$('#downloadResult').show();
-					
-				// dataset name is not required
-                 if($("#datasetName").val()==null||$("#datasetName").val().length<0){
-                    $.ajax({
-						url: ColFusionServerUrl+"/HDataverse/DataverseName/"+$("#dataverseFile").val()+"/"+$("#dataverseName").val(),
-  
-                        type: 'POST',
-                        dataType: 'text',
-                        contentType: "application/json",
-                        crossDomain: true,
-                        success: function(data) {
+/**
+ * Search files on the Harvard Dataverse
+ * @return {[type]} [description]
+ */
+function searchDataverse() {
+    debugger;
+	$('#searchField').hide();
+	$('#searchResult').show();
+	
+    if($('#filenum').text().indexOf("no") > -1){
+		$('#uploadbtn').hide();
+	}
 
-                            var json = JSON.parse(data);
-                            $('#filenum').text("There are "+json.length+" files.");
-                            $('<p> </p>' ).appendTo($('#filenum'));
-                           
-                            for(i=0;i<json.length;i++){
-                                var obj=JSON.parse(json[i]);
-                                var id=obj.file_id;
-                                var name=obj.name;
-                                var radioBtn = $('<input type="radio" name="fileid" value="' + id + '" data-value="'+name+'"/>');
-                      
-                                var s=" FILE NAME: "+obj.name+";    SIZE: "+obj.size_in_bytes+";    INFORMATION: "+obj.dataset_citation+"</br>";
-                               
-                                radioBtn.appendTo($('#filenum'));
-                                $('#filenum').append(s);  
-                           }
-						   $('#uploadbtn').show();
+	if($('#filenum').text().indexOf("0") > -1){
+		$('#uploadbtn').hide();
+	}
 
-                        },
-                        error: function(data) {
-                            alert("Something went wrong while getting services' list. Please try again.");
-                        }
-                    });
-                 }
-                 else{
-    				$.ajax({
-						url: ColFusionServerUrl+"/HDataverse/DataverseName/"+$("#dataverseFile").val()+"/"+$("#dataverseName").val()+"/"+$("#datasetName").val(),
-    					
-    					type: 'POST',
-    					dataType: 'text',
-    					contentType: "application/json",
-    					crossDomain: true,
-    					success: function(data) {
-                            var json = JSON.parse(data);
-                            $('#filenum').text("There are "+json.length+" files.");
-                            $('<p>  </p>' ).appendTo($('#filenum'));
-                            for(i=0;i<json.length;i++){
-                                var obj=JSON.parse(json[i]);
-                                var id=obj.file_id;
-                                var name=obj.name;
-                                var radioBtn = $('<input type="radio" name="fileid" value="' + id + '" data-value="'+name+'"/>');
-          
-                                var s=" FILE NAME: "+obj.name+";    SIZE: "+obj.size_in_bytes+";    INFORMATION: "+obj.dataset_citation+"</br>";
-                                
-                                radioBtn.appendTo($('#filenum'));
-                                $('#filenum').append(s);
-                            
-                           }
-						   $('#uploadbtn').show();
-                            
-    					},
-    					error: function(data) {
-    						alert("Something went wrong while getting services' list. Please try again.");
-    					}
-    				});
-                }
-				
-			}
+	$('#downloadResult').show();
 
-			
+    var fileName = $("#dataverseFile").val();
+    var dataverseName = $("#dataverseName").val();
+    var datasetName = $("#datasetName").val();
+
+	$.ajax({
+		url: restApis.getDataverseSearch(fileName, dataverseName, datasetName),	
+		type: 'GET',
+		dataType: 'application/json',
+		success: function(data) {
+            var json = JSON.parse(data);
+            $('#filenum').text("There are " + json.length + " files.");
+            $('<p>  </p>' ).appendTo($('#filenum'));
+            for (i = 0; i < json.length; i++){
+                var obj = JSON.parse(json[i]);
+                var id = obj.file_id;
+                var name = obj.name;
+                var radioBtn = $('<input type="radio" name="fileid" value="' + id + '" data-value="' + name + '"/>');
+
+                var s = " FILE NAME: " + obj.name + ";    SIZE: " + obj.size_in_bytes + ";    INFORMATION: " + obj.dataset_citation + "</br>";
+                
+                radioBtn.appendTo($('#filenum'));
+                $('#filenum').append(s);            
+           }
+
+		   $('#uploadbtn').show();    
+		},
+		error: function(data) {
+			alert("Something went wrong while getting services' list. Please try again.");
+		}
+	});
+}			
